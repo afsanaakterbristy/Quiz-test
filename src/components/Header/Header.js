5e7c13a3639f8e9d0689fbbd5d9f446eaa68476e
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-10-10 21:44:42 +0600

    added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='header'>
+                <div className='quiz-title'>
+                        <h2>Assigment Quiz Test</h2>
+                </div>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/quizs'>Allquiz</Link>
+                <Link to='/statisticschart'>StatisticsChart</Link>
+                <Link to='/blog'>Blog</Link>
+            </div>
+        </nav>
+      </div>
+    );
+};
+
+export default Header;
