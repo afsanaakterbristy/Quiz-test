74c5d65eee19861e778983887101a2a2ab48b392
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-10-11 12:36:38 +0600

    added

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Option.css'
-
-const Option = ({option}) => {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Option = ({ option,correctAnswer,id }) => {
+    //console.log(correctAnswer);
+    const handleCorrectAnswer = (option) => {
+        if (option === correctAnswer) {
+            toast.success("Wow Your Answer Ccorrect!");
+        } else {
+             toast.warn("Sorry Wrong Answer !");
+        }
+    }
     return (
         <div className='option-container'>
             <div>
-                   <input className='option' type="radio" id="html" name="fav_language" value="HTML"/>
-           <label className='' for="html">{option}</label>
+                   <input onChange={()=>handleCorrectAnswer(option)} className='option' type="radio" id={option} name="option" value={option}/>
+                <label className='' htmlFor={option}> {option}</label>
+                <ToastContainer
+                position="top-center"
+                ></ToastContainer>
          </div>
         </div>
     );
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -3,14 +3,16 @@ import Option from '../Option/Option';
 import './QuizeQuestion.css'
 
 const QuizQuestion = ({quizQuestion }) => {
-   const {options,question,id}=quizQuestion
-    //console.log(id);
+   const {options,question,id,correctAnswer}=quizQuestion
+    console.log(quizQuestion);
     return (
         <div className='question-container'>
-            <h3>{question}</h3>
+             <h3>{question}</h3>
+                <button className='btn-eye'>ok</button>
+
             <div className='option'>
                   {
-                options.map(option => <Option key={option.id } option={option}></Option>)
+                options.map(option => <Option key={option.id } option={option} correctAnswer={correctAnswer}></Option>)
             }
           </div>
 
