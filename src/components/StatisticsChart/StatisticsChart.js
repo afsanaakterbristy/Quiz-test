dee4408f1e2b9aa7003ef1c64160190d0fb33c39
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-10-11 14:26:35 +0600

    added

--- src/components/StatisticsChart/StatisticsChart.js
+++ src/components/StatisticsChart/StatisticsChart.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import './StatisticChart.css'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const StatisticsChart = () => {
     const statistics = useLoaderData().data
     //console.log(statistics.data);
     return (
-        <div>
+         <div className='chart'>
+             <ResponsiveContainer width="100%" height={400}>
             <LineChart width={500} height={400} data={statistics}>
                 <Line type="monotone" dataKey="total" stroke="#82ca9d" />
                   <XAxis dataKey="name" />
-                    <YAxis />
-            </LineChart>
+                <YAxis />
+                <Tooltip></Tooltip>
+                </LineChart>
+                </ResponsiveContainer>
         </div>
     );
 };
